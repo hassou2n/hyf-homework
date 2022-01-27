4b9359f84c4537db62241e48f9159ad5d884a9b4
H O <ho@Hs-MacBook-Pro.local>
2022-01-27 16:49:38 +0100

    add code main.js

--- javascript/javascript2/week1/hyfBya/main.js
+++ javascript/javascript2/week1/hyfBya/main.js
@@ -1,18 +1,25 @@
-body {
-    font-family: "Open Sans", sans-serif;
-    background-color: #F9FBFD;
-}
+console.log("Script loaded");
 
-* {
-    box-sizing: border-box;
-}
+const products = getAvailableProducts();
+console.log(products);
 
-body, h1, h2 {
-    margin: 0;
-}
+const pUl = document.createElement("ul");
+pUl.classList.add("products-list");
+document.querySelector("main").appendChild(pUl);
 
-ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
+function renderProducts(products) {
+  for (let i = 0; i < products.length; i++) {
+    let product = document.createElement("li");
+    product.innerHTML =
+      "<h1>" +
+      products[i].name +
+      "</h1><em> price: " +
+      products[i].price +
+      "</em><em> rating: " +
+      products[i].rating +
+      "</em>";
+
+    pUl.appendChild(product);
+  }
 }
+renderProducts(products);
