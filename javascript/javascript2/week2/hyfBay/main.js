87e5809c48a3d55d3acee362f993dfca5409f17b
H O <ho@Hs-MacBook-Pro.local>
2022-02-12 13:44:15 +0100

    U

--- javascript/javascript2/week2/hyfBay/main.js
+++ javascript/javascript2/week2/hyfBay/main.js
@@ -2,16 +2,6 @@ console.log("Script loaded");
 
 const products = getAvailableProducts();
 
-const filter = document.getElementById("filter");
-let h3 = document.getElementsByName("h3");
-filter.addEventListener("keyup", (e) => {
-  const srarchh = e.target.value;
-  const fil = h3.filter((p) => {
-    return p.includes(srarchh);
-  });
-  console.log(fil);
-});
-
 const pUl = document.createElement("ul");
 pUl.classList.add("products-list");
 document.querySelector("main").appendChild(pUl);
@@ -20,7 +10,7 @@ function renderProducts(products) {
   for (let i = 0; i < products.length; i++) {
     let product = document.createElement("li");
     product.innerHTML = `
-      <h3> Title: ${products[i].name} </h3>
+      <h3>${products[i].name} </h3>
       <p> price: ${products[i].price} </p>
       <p> rating: ${products[i].rating} </p>`;
 
@@ -28,3 +18,30 @@ function renderProducts(products) {
   }
 }
 renderProducts(products);
+
+let filterInput = document.querySelector(".filterInput");
+console.log(filterInput);
+
+filterInput.addEventListener("input", () => {
+  const filterByName = products.filter((el) => {
+    return el.name.toLowerCase().includes(filterInput.value);
+  });
+  pUl.innerHTML = "";
+  renderProducts(filterByName);
+});
+
+let filterInputPrice = document.querySelector(".filterInputPrice");
+
+filterInputPrice.addEventListener("input", () => {
+  const filterPrice = products.filter((el) => {
+    let colenProducts = { ...products };
+
+    if (filterinputp.value > 0) {
+      return el.price < filterPrice.value;
+    } else if (filterPrice.value == 0) {
+      return colenProducts;
+    }
+  });
+  pUl.innerHTML = "";
+  renderProducts(filterPrice);
+});
