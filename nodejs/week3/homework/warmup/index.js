41580b450a70a145ac1b272b280c17efed8f78c6
H O <ho@Hs-MacBook-Pro.local>
2022-04-18 00:49:35 +0200

    Edit

--- nodejs/week3/homework/warmup/index.js
+++ nodejs/week3/homework/warmup/index.js
@@ -3,30 +3,45 @@ const app = express();
 
 app.get("/", (req, res) => res.send("nodejs week3 homework"));
 
-
-
 app.get("/calculator/add", (req, res) => {
-    const firstNumber = Number(req.query.first);
-    const secondNumber = Number(req.query.second);
-    res.send(`${firstNumber + secondNumber}`);
-  });
+  const firstNumber = Number(req.query.firstParam);
+  const secondNumber = Number(req.query.secondParam);
+  if (firstNumber == 0) {
+    res.send(`firstNumber is not valid`)
+  } else if (secondNumber == 0) {
+    res.send(`secondNumber is not valid`)
+  } else { res.send(`${firstNumber + secondNumber}`) }
+});
 
-  app.get("/calculator/Subtract", (req, res) => {
-    const firstNumber = Number(req.query.first);
-    const secondNumber = Number(req.query.second);
-    res.send(`${firstNumber - secondNumber}`);
-  });
+app.get("/calculator/Subtract", (req, res) => {
+  const firstNumber = Number(req.query.firstParam);
+  const secondNumber = Number(req.query.secondParam);
+  if (firstNumber == 0) {
+    res.send(`firstNumber is not valid`)
+  } else if (secondNumber == 0) {
+    res.send(`secondNumber is not valid`)
+  } else { res.send(`${firstNumber - secondNumber}`) }
+});
 
-  app.post("/calculator/multiply", (req, res) => {
-    const firstNumber = Number(req.query.first);
-    const secondNumber = Number(req.query.second);
-    res.send(`${firstNumber * secondNumber}`);
-  });
 
-  app.get("/calculator/div", (req, res) => {
-    const firstNumber = Number(req.query.first);
-    const secondNumber = Number(req.query.second);
-    res.send(`${firstNumber / secondNumber}`);
-  });
-  
+app.post("/calculator/multiply", (req, res) => {
+  const firstNumber = Number(req.query.firstParam);
+  const secondNumber = Number(req.query.secondParam);
+  if (firstNumber == 0) {
+    res.send(`firstNumber is not valid`)
+  } else if (secondNumber == 0) {
+    res.send(`secondNumber is not valid`)
+  } else { res.send(`${firstNumber * secondNumber}`) }
+});
+
+app.get("/calculator/div", (req, res) => {
+  const firstNumber = Number(req.query.firstParam);
+  const secondNumber = Number(req.query.secondParam);
+  if (firstNumber == 0) {
+    res.send(`firstNumber is not valid`)
+  } else if (secondNumber == 0) {
+    res.send(`secondNumber is not valid`)
+  } else { res.send(`${firstNumber / secondNumber}`) }
+});
+
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
